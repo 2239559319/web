b2e1c8658e18c13346e8801a68090de6fc6ddd35
2239559319 <2017141461249@stu.scu.edu.cn>
2018-05-25 19:09:51 +0800

    String对象

---
+++ js/4/4.js
@@ -0,0 +1,18 @@
+/*
+内置对象 String对象
+
+*/
+var str1 = "this";
+var str2 = "this";//这两种表示方法完全相同
+console.log(str2.length)
+console.log(str1[0]);
+
+//在html中的显示
+console.log(str1.big())
+/*
+方法
+toLowerCase()大写转小写
+toUpperCase()小写转大写
+charAt(位置)返回一个字符
+substring(,)返回截取的子串
+*/
